5e510364d3842e277e54624a55b171ae4aec0609
SamuelBrinker <44100703+SamuelBrinker@users.noreply.github.com>
2019-08-19 14:45:15 -0700

    Add files via upload

--- lib/samuelbrinkerfunctional_enrichment_update/Utils/FunctionalEnrichmentUtil.py
+++ lib/samuelbrinkerfunctional_enrichment_update/Utils/FunctionalEnrichmentUtil.py
@@ -18,7 +18,7 @@ from rpy2.robjects.vectors import FloatVector
 #from GenomeFileUtil.GenomeFileUtilClient import GenomeFileUtil
 from installed_clients.GenomeSearchUtilClient import GenomeSearchUtil
 #from DataFileUtil.DataFileUtilClient import DataFileUtil
-#from KBaseReport.KBaseReportClient import KBaseReport
+from installed_clients.KBaseReportClient import KBaseReport
 #from Workspace.WorkspaceClient import Workspace as Workspace
 from installed_clients.DataFileUtilClient import DataFileUtil
 from installed_clients.WorkspaceClient import Workspace
